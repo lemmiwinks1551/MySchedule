2570bae110f5b58d402b727a40c81e3eef37b0cd
kirill <ananevka@mail.ru>
2022-06-14 22:54:15 +0300

    v.0.0.2

--- app/src/main/java/com/example/projectnailsschedule/MainActivity.kt
+++ app/src/main/java/com/example/projectnailsschedule/MainActivity.kt
@@ -26,8 +26,8 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
-        binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        binding.appBarMain.add.setOnClickListener { view ->
+            Snackbar.make(view, "Здесь будет кнопка Добавить", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
         val drawerLayout: DrawerLayout = binding.drawerLayout
