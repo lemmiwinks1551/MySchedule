fcaeb56075569171af9514ffc0e502d25430c2bd
kirill <ananevka@mail.ru>
2022-06-17 18:54:01 +0300

    v.0.0.2

--- app/src/main/java/com/example/projectnailsschedule/ui/calendar/CalendarFragment.kt
+++ app/src/main/java/com/example/projectnailsschedule/ui/calendar/CalendarFragment.kt
@@ -1,9 +1,11 @@
 package com.example.projectnailsschedule.ui.calendar
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -11,31 +13,34 @@ import com.example.projectnailsschedule.databinding.FragmentCalendarBinding
 
 class CalendarFragment : Fragment() {
 
+    //
     private var _binding: FragmentCalendarBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(CalendarViewModel::class.java)
+        Log.e("LifeCycle", "CalendarFragment created")
+        // Создаем переменную ViewModel
+        val calendarViewModel = ViewModelProvider(this).get(CalendarViewModel::class.java)
 
         _binding = FragmentCalendarBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
+        calendarViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        val calendar: CalendarView = binding.calendar
+        calendar.date = 1
         return root
     }
 
     override fun onDestroyView() {
+        Log.e("LifeCycle", "CalendarFragment onDestroy")
         super.onDestroyView()
         _binding = null
     }
--- app/src/main/java/com/example/projectnailsschedule/ui/calendar/CalendarViewModel.kt
+++ app/src/main/java/com/example/projectnailsschedule/ui/calendar/CalendarViewModel.kt
@@ -1,13 +1,21 @@
 package com.example.projectnailsschedule.ui.calendar
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class CalendarViewModel : ViewModel() {
-
+    init {
+        Log.e("LifeCycle", "CalendarViewModel created")
+    }
     private val _text = MutableLiveData<String>().apply {
         value = "Здесь будет календарь"
     }
     val text: LiveData<String> = _text
+
+    override fun onCleared() {
+        Log.e("LifeCycle", "CalendarViewModel cleared")
+        super.onCleared()
+    }
 }
