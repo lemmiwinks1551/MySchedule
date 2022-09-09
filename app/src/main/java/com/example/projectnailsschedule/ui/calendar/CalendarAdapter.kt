3a4a5d8d291ff891c9c5f1dc4b53e05a3d2c43ad
kirill <ananevka@mail.ru>
2022-09-09 15:42:54 +0300

    v.1.0.0

--- app/src/main/java/com/example/projectnailsschedule/ui/calendar/CalendarAdapter.kt
+++ app/src/main/java/com/example/projectnailsschedule/ui/calendar/CalendarAdapter.kt
@@ -72,6 +72,11 @@ internal class CalendarAdapter(
         return daysOfMonth.size
     }
 
+    interface OnItemListener {
+        // Подключаем интерфейс onItemListener
+        fun onItemClick(position: Int, dayText: String?)
+    }
+
     companion object {
         // Адаптер работает с ViewHolder`ом
         var month = 0
