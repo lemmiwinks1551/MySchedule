3e38bb4ae9a1accd641c79aeaa6ee58486d7b1b7
kirill <ananevka@mail.ru>
2022-09-10 13:12:44 +0300

    v.1.0.0

--- app/src/main/java/com/example/projectnailsschedule/ui/dataShort/DateShorGetDbData.kt
+++ app/src/main/java/com/example/projectnailsschedule/ui/dataShort/DateShorGetDbData.kt
@@ -3,6 +3,7 @@ package com.example.projectnailsschedule.ui.dataShort
 import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
+import com.example.projectnailsschedule.DatabaseHelper
 import com.example.projectnailsschedule.dateStatusDB.DateStatusDbHelper
 
 /** Получает данные из БД за выбранную дату
@@ -14,11 +15,17 @@ class DateShorGetDbData {
     private lateinit var context: Context
     private lateinit var dbDate: SQLiteDatabase
     private lateinit var cursor: Cursor
+    private lateinit var databaseHelper: DatabaseHelper
 
 
-fun getDataRows(): Int {
-    // Возвращает количество строк в курсоре
-    return cursor.count
-}
+    fun fetchDate() {
+        dbDate = databaseHelper.readableDatabase
+
+    }
+
+    fun getDataRows(): Int {
+        // Возвращает количество строк в курсоре
+        return cursor.count
+    }
 
 }
