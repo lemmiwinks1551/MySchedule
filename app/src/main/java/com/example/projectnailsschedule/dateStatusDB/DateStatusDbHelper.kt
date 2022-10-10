48aa0df5a391b132c8f2d83d9d8d2d3df68d4af8
kirill <ananevka@mail.ru>
2022-10-10 23:25:08 +0300

    v.1.0.1

--- app/src/main/java/com/example/projectnailsschedule/dateStatusDB/DateStatusDbHelper.kt
+++ app/src/main/java/com/example/projectnailsschedule/dateStatusDB/DateStatusDbHelper.kt
@@ -4,8 +4,11 @@ import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.os.Environment
 import android.util.Log
-import com.example.projectnailsschedule.DatabaseHelper
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
 
 /**
  * Методы для взаимодействия с БД по статусам дней календаря:
@@ -33,6 +36,18 @@ class DateStatusDbHelper(context: Context?) :
         const val STATUS_MEDIUM = "medium"
         const val STATUS_BUSY = "busy"
         const val STATUS_DAY_OFF = "dayOff"
+    }
+
+    fun createDbDir() {
+        // TODO: Добавить создание папки, а в неё потом будем класть файл с БД 
+        val dir: File = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+        val file = File(dir, "example.txt")
+
+        try {
+            FileWriter(file).use { fileWriter -> fileWriter.append("Writing to file!") }
+        } catch (e: IOException) {
+            Log.e(LOG, e.toString())
+        }
     }
 
     fun addDate(date: String, status: String, db: SQLiteDatabase) {
