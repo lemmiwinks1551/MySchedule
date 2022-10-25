0f973bd9fd4e898378bc77177b8c36b91251cf1c
kirill <ananevka@mail.ru>
2022-10-25 22:20:18 +0300

    v.1.0.1

--- app/src/main/java/com/example/projectnailsschedule/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/projectnailsschedule/ui/search/SearchFragment.kt
@@ -1,5 +1,6 @@
 package com.example.projectnailsschedule.ui.search
 
+import android.content.Intent
 import android.database.Cursor
 import android.database.SQLException
 import android.database.sqlite.SQLiteDatabase
@@ -13,8 +14,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import android.widget.AdapterView.OnItemClickListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.projectnailsschedule.DateActivity
 import com.example.projectnailsschedule.R
 import com.example.projectnailsschedule.databinding.FragmentSearchBinding
 import com.example.projectnailsschedule.dbHelpers.ScheduleDbHelper
@@ -47,7 +50,14 @@ class SearchFragment : Fragment() {
     override fun onResume() {
         super.onResume()
 
-        userSearch.inputType = InputType.TYPE_CLASS_PHONE
+        userList.onItemClickListener = OnItemClickListener { arg0, arg1, position, arg3 ->
+            val date = (arg0.adapter as SimpleCursorAdapter).cursor.getString(1)
+            val intent = Intent(context, DateActivity::class.java)
+            intent.putExtra("day", date)
+            startActivity(intent)
+        }
+
+
         userSearch.addTextChangedListener(PhoneNumberFormattingTextWatcher())
         toggleButton?.setOnCheckedChangeListener { _, b ->
             if (b) {
