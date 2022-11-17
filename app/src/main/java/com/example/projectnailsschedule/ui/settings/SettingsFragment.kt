a535323fe0dd0e1fc07f8afde65ebcde5b766423
kirill <ananevka@mail.ru>
2022-11-17 18:55:27 +0300

    v.1.0.2

--- app/src/main/java/com/example/projectnailsschedule/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/projectnailsschedule/ui/settings/SettingsFragment.kt
@@ -4,14 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.projectnailsschedule.databinding.FragmentClientsBinding
+import com.example.projectnailsschedule.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
 
-    private var _binding: FragmentClientsBinding? = null
+    private var _binding: FragmentSettingsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,14 +26,21 @@ class SettingsFragment : Fragment() {
         val clientsViewModel =
             ViewModelProvider(this)[SettingsViewModel::class.java]
 
-        _binding = FragmentClientsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
 
-        val textView: TextView = binding.textGallery
-        clientsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        // Set listener om theme switch
+        binding.darkThemeSwitch.setOnClickListener {
+            if (binding.darkThemeSwitch.isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                Toast.makeText(context, "Темная тема активирована", Toast.LENGTH_SHORT).show()
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                Toast.makeText(context, "Темная тема активирована", Toast.LENGTH_SHORT).show()
+
+            }
         }
-        return root
+
+        return binding.root
     }
 
     override fun onDestroyView() {
