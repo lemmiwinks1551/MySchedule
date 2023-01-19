55c3927d2143fdd8b2fa7ad5152d839dff9df3c1
kirill <ananevka@mail.ru>
2023-01-19 19:17:46 +0300

    v.1.0.3

--- app/src/main/java/com/example/projectnailsschedule/data/repository/AppointmentRepository.kt
+++ app/src/main/java/com/example/projectnailsschedule/data/repository/AppointmentRepository.kt
@@ -1,4 +1,12 @@
 package com.example.projectnailsschedule.data.repository
 
 class AppointmentRepository {
+
+    fun saveAppointment(): Boolean {
+        return true
+    }
+
+    fun getAppointment(): Boolean {
+        return true
+    }
 }
---
+++ app/src/main/java/com/example/projectnailsschedule/domain/models/StatusModel.kt
@@ -0,0 +1,4 @@
+package com.example.projectnailsschedule.domain.models
+
+class StatusModel {
+}
