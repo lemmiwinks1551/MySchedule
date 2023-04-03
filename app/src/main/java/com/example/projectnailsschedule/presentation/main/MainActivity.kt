80cbc5f27a4fd4c48d09fd529e573a6af0548175
Kirill <ananevka@mail.ru>
2023-04-03 19:08:14 +0300

    v.1.0.4

--- app/src/main/java/com/example/projectnailsschedule/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/projectnailsschedule/presentation/main/MainActivity.kt
@@ -46,21 +46,6 @@ class MainActivity : AppCompatActivity() {
         exitProcess(0)
     }
 
-    private fun showInContextUI() {
-        AlertDialog.Builder(this)
-            .setTitle("Delete entry")
-            .setMessage("Are you sure you want to delete this entry?") // Specifying a listener allows you to take an action before dismissing the dialog.
-            // The dialog is automatically dismissed when a dialog button is clicked.
-            .setPositiveButton(
-                android.R.string.yes
-            ) { dialog, which ->
-                // Continue with delete operation
-            } // A null listener allows the button to dismiss the dialog and take no further action.
-            .setNegativeButton(android.R.string.no, null)
-            .show()
-        closeApp()
-    }
-
     @RequiresApi(Build.VERSION_CODES.M)
     private fun permission() {
 
