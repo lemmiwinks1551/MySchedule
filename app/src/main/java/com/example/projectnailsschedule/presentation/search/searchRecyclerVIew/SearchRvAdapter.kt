3d4dccba12aeba60cb5879df40ad0c350458c2e1
Kirill <ananevka@mail.ru>
2023-08-15 15:39:46 +0300

    v.6.4(19)

--- app/src/main/java/com/example/projectnailsschedule/presentation/search/searchRecyclerVIew/SearchRvAdapter.kt
+++ app/src/main/java/com/example/projectnailsschedule/presentation/search/searchRecyclerVIew/SearchRvAdapter.kt
@@ -6,9 +6,11 @@ import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.projectnailsschedule.R
 import com.example.projectnailsschedule.domain.models.AppointmentModelDb
+import com.example.projectnailsschedule.presentation.date.dateRecyclerView.DateViewHolder
 import com.example.projectnailsschedule.presentation.search.SearchViewModel
 
 internal class SearchRvAdapter(
@@ -57,6 +59,10 @@ internal class SearchRvAdapter(
             procedure.text = appointmentModelDb.procedure
             name.text = appointmentModelDb.name
             phone.text = appointmentModelDb.phone
+            vkLink.text = appointmentModelDb.vk
+            telegramLink.text = appointmentModelDb.telegram
+            instagramLink.text = appointmentModelDb.instagram
+            whatsAppLink.text = appointmentModelDb.whatsapp
             notes.text = appointmentModelDb.notes
         }
 
@@ -65,6 +71,8 @@ internal class SearchRvAdapter(
             val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phone"))
             context.startActivity(intent)
         }
+
+        initSocClickListeners(holder)
     }
 
     override fun getItemCount(): Int {
@@ -72,5 +80,113 @@ internal class SearchRvAdapter(
         searchViewModel.appointmentsTotalCount.value = appointmentCount
         searchViewModel.getAllAppointmentsLiveData()
         return appointmentCount
+    }
+
+    private fun initSocClickListeners(holder: SearchViewHolder) {
+        with(holder) {
+            vkImageButton.setOnClickListener {
+                startVk(holder.vkLink.text.toString().trim())
+            }
+
+            telegramImageButton.setOnClickListener {
+                startTelegram(holder.telegramLink.text.toString().trim())
+            }
+
+            instagramImageButton.setOnClickListener {
+                startInstagram(holder.instagramLink.text.toString().trim())
+            }
+
+            whatsappImageButton.setOnClickListener {
+                startWhatsapp(holder.whatsAppLink.text.toString().trim())
+            }
+        }
+    }
+
+    private fun startVk(uri: String) {
+        val uri = uri.replace("https://vk.com/", "")
+        try {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://vk.com/$uri"))
+            context.startActivity(intent)
+        } catch (e: Exception) {
+            try {
+                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://vk.com/$uri"))
+                context.startActivity(browserIntent)
+            } catch (e: Exception) {
+                Toast.makeText(
+                    context,
+                    "Не удалось перейти во Вконтакте",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    private fun startTelegram(uri: String) {
+        val uri = uri.replace("https://t.me/", "")
+        try {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("tg://resolve?domain=$uri"))
+            context.startActivity(intent)
+        } catch (e: Exception) {
+            try {
+                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://t.me/$uri"))
+                context.startActivity(browserIntent)
+            } catch (e: Exception) {
+                Toast.makeText(
+                    context,
+                    "Не удалось перейти в Telegram",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    private fun startInstagram(uri: String) {
+        val regex = Regex("https:\\/\\/instagram\\.com\\/([^?\\/]+)(?:\\?igshid=.*)?|([\\w.-]+)")
+        val matchResult = regex.find(uri)
+        var username = matchResult?.groupValues?.getOrNull(1)
+        if (username == "") {
+            username = matchResult?.groupValues?.getOrNull(2)
+        }
+
+        try {
+            val intent =
+                Intent(Intent.ACTION_VIEW, Uri.parse("http://instagram.com/_u/$username"))
+            intent.setPackage("com.instagram.android")
+            context.startActivity(intent)
+        } catch (e: Exception) {
+            try {
+                val browserIntent =
+                    Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/$username"))
+                context.startActivity(browserIntent)
+            } catch (e: Exception) {
+                Toast.makeText(
+                    context,
+                    "Не удалось перейти в Instagram",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+    }
+
+    private fun startWhatsapp(uri: String) {
+        try {
+            val intent =
+                Intent(Intent.ACTION_VIEW, Uri.parse("https://api.whatsapp.com/send?phone=$uri"))
+            context.startActivity(intent)
+        } catch (e: Exception) {
+            try {
+                val browserIntent = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://web.whatsapp.com/send?phone=$uri")
+                )
+                context.startActivity(browserIntent)
+            } catch (e: Exception) {
+                Toast.makeText(
+                    context,
+                    "Не удалось перейти в Whatsapp",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/projectnailsschedule/presentation/search/searchRecyclerVIew/SearchViewHolder.kt
+++ app/src/main/java/com/example/projectnailsschedule/presentation/search/searchRecyclerVIew/SearchViewHolder.kt
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.projectnailsschedule.R
 import com.example.projectnailsschedule.domain.models.AppointmentModelDb
 import com.example.projectnailsschedule.presentation.search.SearchFragment
+import org.w3c.dom.Text
 
 class SearchViewHolder internal constructor(
     itemView: View,
@@ -25,16 +26,36 @@ class SearchViewHolder internal constructor(
     var time: TextView
     var notes: TextView
     var procedure: TextView
+
+    var vkLink: TextView
+    var telegramLink: TextView
+    var instagramLink: TextView
+    var whatsAppLink: TextView
+
     var callClientButton: ImageButton
+    var vkImageButton: ImageButton
+    var telegramImageButton: ImageButton
+    var instagramImageButton: ImageButton
+    var whatsappImageButton: ImageButton
 
     init {
         name = itemView.findViewById(R.id.client_value_search)
         date = itemView.findViewById(R.id.date_value_search)
         phone = itemView.findViewById(R.id.phone_value_search)
         time = itemView.findViewById(R.id.time_value_search)
-        notes = itemView.findViewById(R.id.misc_value_search)
+        notes = itemView.findViewById(R.id.client_notes)
         procedure = itemView.findViewById(R.id.procedure_value_search)
+
+        vkLink = itemView.findViewById(R.id.client_vk_link_tv)
+        telegramLink = itemView.findViewById(R.id.client_telegram_link_tv)
+        instagramLink = itemView.findViewById(R.id.client_instagram_link_tv)
+        whatsAppLink = itemView.findViewById(R.id.client_whatsapp_link_tv)
+
         callClientButton = itemView.findViewById(R.id.call_client_button)
+        vkImageButton = itemView.findViewById(R.id.vk_logo_imageButton_date)
+        telegramImageButton = itemView.findViewById(R.id.telegram_logo_imageButton_date)
+        instagramImageButton = itemView.findViewById(R.id.instagram_logo_imageButton_date)
+        whatsappImageButton = itemView.findViewById(R.id.whatsapp_logo_imageButton_date)
 
         itemView.setOnClickListener {
             listener.onItemClick(adapterPosition)
