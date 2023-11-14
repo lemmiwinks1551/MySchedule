5f98af0cec421fb5b14d21650c5c7fc0a1dafa41
Kirill <ananevka@mail.ru>
2023-11-14 21:14:39 +0300

    v.7.2(29)

---
+++ app/src/test/java/com/example/projectnailsschedule/domain/usecase/clientsUC/DeleteClientUseCaseTest.kt
@@ -0,0 +1,21 @@
+package com.example.projectnailsschedule.domain.usecase.clientsUC
+
+import com.example.projectnailsschedule.domain.models.ClientModelDb
+import com.example.projectnailsschedule.domain.repository.ClientsRepository
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+
+internal class DeleteClientUseCaseTest {
+    private val clientsRepository = mock<ClientsRepository>()
+
+    @Test
+    fun `should delete Client from Clients repository and return true`() {
+        val deleteClientUseCase = DeleteClientUseCase(clientsRepository = clientsRepository)
+        val testClient = ClientModelDb()
+        val expected = true
+        val actual = deleteClientUseCase.execute(testClient)
+
+        Assertions.assertEquals(expected, actual)
+    }
+}
