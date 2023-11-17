30bbafadd53f1c635135c7e4a6cff53b90c18cce
Kirill <ananevka@mail.ru>
2023-11-17 16:04:44 +0300

    v.7.2(29)

---
+++ app/src/test/java/com/example/projectnailsschedule/domain/usecase/settingsUC/GetLanguageUseCaseTest.kt
@@ -0,0 +1,22 @@
+package com.example.projectnailsschedule.domain.usecase.settingsUC
+
+import com.example.projectnailsschedule.domain.repository.SettingsRepository
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.mockito.kotlin.mock
+
+internal class GetLanguageUseCaseTest {
+    private val settingsRepository = mock<SettingsRepository>()
+
+    @Test
+    fun `should return Language from Settings repository and return String`() {
+        val getLanguageUseCase = GetLanguageUseCase(settingsRepository = settingsRepository)
+        val expected = "English"
+
+        Mockito.`when`(settingsRepository.getLanguage()).thenReturn(expected)
+        val actual = getLanguageUseCase.execute()
+
+        Assertions.assertEquals(expected, actual)
+    }
+}
