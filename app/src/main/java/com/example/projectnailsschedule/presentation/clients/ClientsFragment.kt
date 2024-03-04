2bf87ab0cc3fe9fe28585cf1667d788eaed38873
Kirill <ananevka@mail.ru>
2024-03-04 18:53:33 +0300

    v.7.3.39

--- app/src/main/java/com/example/projectnailsschedule/presentation/clients/ClientsFragment.kt
+++ app/src/main/java/com/example/projectnailsschedule/presentation/clients/ClientsFragment.kt
@@ -8,7 +8,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.widget.SearchView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
