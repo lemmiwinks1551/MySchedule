da353744f26baa9121a1d861902c8ef42f7e4f57
Kirill <ananevka@mail.ru>
2024-04-26 16:04:10 +0300

    v.7.3.44

--- app/src/main/java/com/example/projectnailsschedule/data/storage/ScheduleDb.kt
+++ app/src/main/java/com/example/projectnailsschedule/data/storage/ScheduleDb.kt
@@ -11,7 +11,14 @@ import com.example.projectnailsschedule.domain.repository.ScheduleDao
 @Database(
     entities = [AppointmentModelDb::class],
     version = 7,
-    autoMigrations = [AutoMigration(from = 6, to = 7)]
+    autoMigrations = [
+        AutoMigration(from = 1, to = 2),
+        AutoMigration(from = 2, to = 3),
+        AutoMigration(from = 3, to = 4),
+        AutoMigration(from = 4, to = 5),
+        AutoMigration(from = 5, to = 6),
+        AutoMigration(from = 6, to = 7)
+    ]
 )
 abstract class ScheduleDb : RoomDatabase() {
     abstract fun getDao(): ScheduleDao
