e1b63a5ab1693e1b954caf67d36f737ae77cf327
Kirill <ananevka@mail.ru>
2024-11-10 21:35:30 +0300

    v.7.3.54

--- app/src/main/java/com/example/projectnailsschedule/data/storage/converters/DateTypeAdapter.kt
+++ app/src/main/java/com/example/projectnailsschedule/data/storage/converters/DateTypeAdapter.kt
@@ -8,7 +8,7 @@ import java.util.Date
 import java.util.Locale
 
 class DateTypeAdapter : TypeAdapter<Date>() {
-    private val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault())
+    private val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault())
 
     override fun write(out: JsonWriter?, value: Date?) {
         if (value != null) {
