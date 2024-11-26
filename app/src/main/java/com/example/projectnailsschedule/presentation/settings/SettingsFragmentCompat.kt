2bcd37449386f517b46b5678a95aba6470498f2d
Kirill <ananevka@mail.ru>
2024-11-26 17:37:43 +0300

    v.7.3.54

--- app/src/main/java/com/example/projectnailsschedule/presentation/settings/SettingsFragmentCompat.kt
+++ app/src/main/java/com/example/projectnailsschedule/presentation/settings/SettingsFragmentCompat.kt
@@ -1,18 +1,28 @@
 package com.example.projectnailsschedule.presentation.settings
 
+import android.app.Dialog
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
 import com.example.projectnailsschedule.R
 import com.example.projectnailsschedule.presentation.settings.faq.FaqDialogFragment
 import com.example.projectnailsschedule.presentation.settings.themes.SelectThemeDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SettingsFragmentCompat : PreferenceFragmentCompat() {
@@ -21,13 +31,15 @@ class SettingsFragmentCompat : PreferenceFragmentCompat() {
     private val selectThemeFragmentTag = "SelectThemeFragment"
     private val faqFragmentTag = "FaqFragment"
 
+    private lateinit var synchronization: SwitchPreference
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
 
-        settingsViewModel.updateUserData("$log ${object{}.javaClass.enclosingMethod?.name}")
+        settingsViewModel.updateUserData("$log ${object {}.javaClass.enclosingMethod?.name}")
 
         return super.onCreateView(inflater, container, savedInstanceState)
     }
@@ -38,6 +50,7 @@ class SettingsFragmentCompat : PreferenceFragmentCompat() {
         val changeTheme = findPreference<Preference>("theme")
         val sendFeedback = findPreference<Preference>("feedback")
         val faq = findPreference<Preference>("faq")
+        synchronization = findPreference("synchronization")!!
 
         changeTheme?.setOnPreferenceClickListener {
             val dialogFragment = SelectThemeDialogFragment()
@@ -55,6 +68,44 @@ class SettingsFragmentCompat : PreferenceFragmentCompat() {
             dialogFragment.show(parentFragmentManager, faqFragmentTag)
             true
         }
+
+        synchronization.setOnPreferenceClickListener {
+            if (synchronization.isChecked) {
+                showDialogSync()
+            }
+            true
+        }
+    }
+
+    private fun showDialogSync() {
+        val dialog = Dialog(requireContext())
+        val view = layoutInflater.inflate(R.layout.dialog_warning_sync, null)
+        dialog.setContentView(view)
+
+        dialog.window?.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+
+        val okButton = view.findViewById<Button>(R.id.ok_button)
+        val cancelButton = view.findViewById<Button>(R.id.cancel_button)
+
+        okButton.setOnClickListener {
+            synchronization.isChecked = true
+            dialog.dismiss()
+        }
+
+        cancelButton.setOnClickListener {
+            synchronization.isChecked = false
+            dialog.dismiss()
+        }
+
+        dialog.setOnCancelListener {
+            synchronization.isChecked = false
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 
     private fun sendEmail() {
@@ -68,6 +119,6 @@ class SettingsFragmentCompat : PreferenceFragmentCompat() {
 
     override fun onDestroyView() {
         super.onDestroyView()
-        settingsViewModel.updateUserData("$log ${object{}.javaClass.enclosingMethod?.name}")
+        settingsViewModel.updateUserData("$log ${object {}.javaClass.enclosingMethod?.name}")
     }
 }
