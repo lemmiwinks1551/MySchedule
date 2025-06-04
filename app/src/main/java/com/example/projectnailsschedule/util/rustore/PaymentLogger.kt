1ff77fa9e2510aa62e79f1c848638202772ae3bd
Kirill <ananevka@mail.ru>
2025-06-04 12:43:16 +0300

    RuStore SDK PaymentLogger.kt

---
+++ app/src/main/java/com/example/projectnailsschedule/util/rustore/PaymentLogger.kt
@@ -0,0 +1,28 @@
+package com.example.projectnailsschedule.util.rustore
+
+import android.util.Log
+import ru.rustore.sdk.billingclient.provider.logger.ExternalPaymentLogger
+
+/** Класс для логирования событий библиотеки платежей */
+
+class PaymentLogger(private val tag: String) : ExternalPaymentLogger {
+    override fun d(e: Throwable?, message: () -> String) {
+        Log.d(tag, message.invoke(), e)
+    }
+
+    override fun e(e: Throwable?, message: () -> String) {
+        Log.e(tag, message.invoke(), e)
+    }
+
+    override fun i(e: Throwable?, message: () -> String) {
+        Log.i(tag, message.invoke(), e)
+    }
+
+    override fun v(e: Throwable?, message: () -> String) {
+        Log.v(tag, message.invoke(), e)
+    }
+
+    override fun w(e: Throwable?, message: () -> String) {
+        Log.w(tag, message.invoke(), e)
+    }
+}
