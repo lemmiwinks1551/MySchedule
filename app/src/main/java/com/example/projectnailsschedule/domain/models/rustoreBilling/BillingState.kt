c2215226de8d1a67795cbf0b6ac8e5adfb108346
Kirill <ananevka@mail.ru>
2025-06-11 09:41:00 +0300

    + purchases: List<Purchase>

--- app/src/main/java/com/example/projectnailsschedule/domain/models/rustoreBilling/BillingState.kt
+++ app/src/main/java/com/example/projectnailsschedule/domain/models/rustoreBilling/BillingState.kt
@@ -2,10 +2,12 @@ package com.example.projectnailsschedule.domain.models.rustoreBilling
 
 import androidx.annotation.StringRes
 import ru.rustore.sdk.billingclient.model.product.Product
+import ru.rustore.sdk.billingclient.model.purchase.Purchase
 
 data class BillingState(
     val isLoading: Boolean = false,
     val products: List<Product> = emptyList(),
+    val purchases: List<Purchase> = emptyList(),
     @StringRes val snackbarResId: Int? = null
 ) {
     val isEmpty: Boolean = products.isEmpty() && !isLoading
