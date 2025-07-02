87c8b24c4a8b94c8983012af1fef82455e1af56b
Kirill <ananevka@mail.ru>
2025-07-02 14:27:39 +0300

    worker cfg

--- app/src/main/java/com/example/projectnailsschedule/app/App.kt
+++ app/src/main/java/com/example/projectnailsschedule/app/App.kt
@@ -1,7 +1,20 @@
 package com.example.projectnailsschedule.app
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class App : Application()
+class App : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var hiltWorkerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration(): Configuration {
+        return Configuration.Builder()
+            .setWorkerFactory(hiltWorkerFactory)
+            .build()
+    }
+}
